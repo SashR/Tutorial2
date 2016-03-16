b88203dbf3eeb2cf8f42abeea7d13aba83c3c50a
Sashin Ramdhani <sashinramdhani@gmail.com>
2016-03-16 13:00:27 +0200

    function declaration

---
+++ Tut2_fraction.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+class fraction
+
+{
+private:
+
+public:
+	int num;
+	int den;
+	void Add(fraction, fraction);
+	void Sub(fraction, fraction);
+	void Multi(fraction, fraction);
+	void Div(fraction, fraction);
+	void setvalues();
+};
